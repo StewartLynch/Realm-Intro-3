a1e96230b9fa31c6fd38c141ea773ae5bda99ad8
Stewart Lynch <slynch@createchsol.com>
2022-03-29 19:18:03 -0700

    Completed Project

--- Realm Intro-3/RealmManager.swift
+++ Realm Intro-3/RealmManager.swift
@@ -12,11 +12,20 @@ class RealmManager: ObservableObject {
     
     private(set) var realm: Realm?
     @Published var countries: Results<Country>?
+    @Published var searchFilter = ""
     var countriesArray: [Country] {
         if let countries = countries {
             return Array(countries)
         } else {
             return []
+        }
+    }
+    
+    var searchResults: [Country] {
+        if searchFilter.isEmpty {
+            return countriesArray
+        } else {
+            return countriesArray.filter{$0.name.lowercased().contains(searchFilter.lowercased())}
         }
     }
     private var countriesToken: NotificationToken?
@@ -52,5 +61,43 @@ class RealmManager: ObservableObject {
             print("Error opening default realm", error)
         }
         
+    }
+    
+    func add(_ country: Country) {
+        if let realm = realm {
+            do {
+                try realm.write {
+                    realm.add(country)
+                }
+            } catch {
+                print("Error adding country to realm", error)
+            }
+        }
+    }
+    
+    func remove(_ country: Country) {
+        if let realm = realm {
+            if let countryToDelete = realm.object(ofType: Country.self, forPrimaryKey: country.id) {
+                do {
+                    try realm.write {
+                        realm.delete(countryToDelete)
+                    }
+                } catch {
+                    print("Could not delete country", error)
+                }
+            }
+        }
+    }
+    
+    func deleteCities(for country: Country) {
+        if let realm = realm {
+            do {
+                try realm.write {
+                    realm.delete(country.cities)
+                }
+            } catch {
+                print("Could not delete cities")
+            }
+        }
     }
 }
--- Realm Intro-3/Viiews/CountriesListView.swift
+++ Realm Intro-3/Viiews/CountriesListView.swift
@@ -9,38 +9,42 @@ import SwiftUI
 import RealmSwift
 
 struct CountriesListView: View {
-    @ObservedResults(Country.self) var countries
+    @EnvironmentObject var realmManager: RealmManager
     @FocusState private var isFocused: Bool?
-    @State private var presentAlert = false
-    @State private var searchFilter = ""
+
     var body: some View {
         NavigationView {
             VStack {
+                if let countries = realmManager.searchResults {
                     List {
-                        ForEach(countries.sorted(byKeyPath: "name")) { country in
-                            NavigationLink {
-                                CitiesListView(country: country)
-                            } label: {
-                                CountryRowView(country: country, isFocused: _isFocused)
+                        ForEach(countries.sorted{$0.name < $1.name}) { country in
+                            if !country.isInvalidated {
+                                NavigationLink {
+                                    CitiesListView(country: country)
+                                } label: {
+                                    CountryRowView(country: country, isFocused: _isFocused)
+                                }
                             }
                         }
                         .onDelete(perform: deleteCountry)
                         .listRowSeparator(.hidden)
                     }
                     .listStyle(.plain)
-//                    .searchable(text: $searchFilter,collection: $countries, keyPath: \.name, placement: .navigationBarDrawer(displayMode: .always)) {
-//                        ForEach(countries) { country in
-//                            Text(country.name)
-//                                .searchCompletion(country.name)
-//                        }
-//                    }
+                    .searchable(text: $realmManager.searchFilter, placement: .navigationBarDrawer(displayMode: .always)) {
+                        ForEach(countries) { country in
+                            Text(country.name)
+                                .searchCompletion(country.name)
+                        }
+                    }
+                }
             }
-            .animation(.default, value: countries)
+            .animation(.default, value: realmManager.countries)
             .navigationTitle("Countries")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
-                        $countries.append(Country())
+//                        $countries.append(Country())
+                        realmManager.add(Country())
                     } label: {
                         Image(systemName: "plus.circle.fill")
                             .font(.title2)
@@ -58,22 +62,18 @@ struct CountriesListView: View {
                 }
             }
         }
-        .alert("You must first delete all of the cities in this country.", isPresented: $presentAlert, actions: {})
     }
     func deleteCountry(indexSet: IndexSet) {
         guard let index = indexSet.first else { return }
-        let selectedCountry = Array(countries.sorted(byKeyPath: "name"))[index]
-        guard selectedCountry.cities.isEmpty else {
-            // show alert
-            presentAlert.toggle()
-            return
-        }
-        $countries.remove(selectedCountry)
+        let selectedCountry = realmManager.countriesArray.sorted {$0.name < $1.name}[index]
+        realmManager.deleteCities(for: selectedCountry)
+        realmManager.remove(selectedCountry)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct CountriesListView_Previews: PreviewProvider {
     static var previews: some View {
         CountriesListView()
+            .environmentObject(RealmManager(name: "sample"))
     }
 }
